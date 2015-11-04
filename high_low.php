ab644639783b2ac068bcca35f2145cc14f179bd5
Sakib Shaikh <sakibshaikh@Sakibs-MacBook-Air.local>
2015-11-04 12:00:33 -0600

    High Low Game

--- high_low.php
+++ high_low.php
@@ -1,6 +1,7 @@
  <?php
 
  $target = rand(1, 100);
+ $tries = 0;
  
  do {
 	// Write the output
@@ -11,13 +12,15 @@
 	$guess = trim(fgets(STDIN));
 	// var_dump(trim($firstName));
 	if ($guess == $target) {
-		fwrite(STDOUT, "You Got It!\n");
+		fwrite(STDOUT, "You Got It!\n" . PHP_EOL);
 	} else if ($guess < $target) {
-		fwrite(STDOUT, "Too Low!\n");
+		fwrite(STDOUT, "Too Low!\n" . PHP_EOL);
 	} else if ($guess > $target) {
-		fwrite(STDOUT, "Too High!\n");
+		fwrite(STDOUT, "Too High!\n" . PHP_EOL);
 	}
-} while ($guess !== $target) 
+	$tries++;
+} while ($guess != $target) 
+echo "It took you $tries number of attempts to guess the number" . PHP_EOL;
 exit(0);
 
 ?>
